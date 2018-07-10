aad456db350a5fec700aa8bff3ba8302584612c1
Austin Gill <notgnoshi@gmail.com>
2018-07-09 19:31:18 -0500

    Style improvements.
    Major:
    * align constants even though our beloved PEP8 forbids it.
    * Add optional timeout to drive_*_wheel commands so that they act the
      same as the drive_forward and drive_backward commands.
    
    Minor:
    * Add comments, examples, solve world hunger, etc., etc.

--- README.md
+++ README.md
@@ -4,3 +4,48 @@ Authors: Kali Regenold, Remington Bullis
 
 This repository will hold the development code for the GenCyber Camp Robotics Class.
 Everyday of the camp, the stable code here will be duplicated into a daily folder in the GenCyber18 repo, aka the production repo.
+
+```python
+import geekbot
+from time import sleep
+
+inigo_montoya = geekbot.Robot(57600)
+if not inigo_montoya.connected:
+    exit()
+
+# Check lights
+inigo_montoya.lights_on()
+sleep(.5)
+inigo_montoya.lights_off()
+
+# Check IR servo and sensor
+inigo_montoya.set_ir_position(0)
+sleep(.25)
+print(inigo_montoya.get_ir_distance())
+sleep(.25)
+inigo_montoya.set_ir_position(90)
+sleep(.25)
+print(inigo_montoya.get_ir_distance())
+sleep(.25)
+inigo_montoya.set_ir_position(-90)
+sleep(.25)
+print(inigo_montoya.get_ir_distance())
+sleep(.25)
+inigo_montoya.set_ir_position(0)
+sleep(.5)
+
+# Check left wheel servo
+inigo_montoya.drive_left_wheel(90, seconds=1)
+inigo_montoya.drive_left_wheel(-90, seconds=1)
+inigo_montoya.drive_left_wheel(0, seconds=0.5)
+
+# Check right wheel servo
+inigo_montoya.drive_right_wheel(90, seconds=1)
+inigo_montoya.drive_right_wheel(-90, seconds=1)
+inigo_montoya.drive_right_wheel(0, seconds=0.5)
+
+# Check drive forward and halt
+inigo_montoya.drive_forward(80, seconds=1)
+
+inigo_montoya.shutdown()
+```
--- geekbot.py
+++ geekbot.py
@@ -5,32 +5,66 @@ from time import sleep as wait
 import serial
 import serial.tools.list_ports
 
-ROBOT_VID = 0x0403
-ROBOT_PID = 0x6001
-ROBOT_PID2 = 0x6015
 
-HANDSHAKE  = 0x77
-END_FLAG   = 0x33
+ROBOT_VID    = 0x0403
+ROBOT_PID    = 0x6001
+ROBOT_PID2   = 0x6015
 
-DRIVE_FLAG = 0x45
-LEFT_FLAG  = 0x36
-RIGHT_FLAG = 0x35
-LIGHTS_FLAG = 0x30
+HANDSHAKE    = 0x77
+END_FLAG     = 0x33
+
+DRIVE_FLAG   = 0x45
+LEFT_FLAG    = 0x36
+RIGHT_FLAG   = 0x35
+LIGHTS_FLAG  = 0x30
 
 IR_READ_FLAG = 0x27
 IR_POS_FLAG  = 0x28
 
+
 class Robot(object):
     """
         A Robot class to interact with a Trossen Robotics Geekbot.
+
+        Example:
+
+        >>> from time import sleep
+        >>> # Robots aren't complete without names.
+        >>> randy = Robot(57600)
+        >>> if not randy.connected:
+        ...     exit()
+        >>> randy.lights_on()
+        >>> sleep(0.5)
+        >>> randy.lights_off()
+        >>> # Drive the left wheel forwards for 1 second.
+        >>> randy.drive_left_wheel(90, seconds=1)
+        >>> # Drive the left wheel backwards for 1.5 seconds.
+        >>> randy.drive_left_wheel(-90, seconds=1.5)
+        >>> # Drive both wheels forward at the same speed for 1 second.
+        >>> randy.drive_forward(80, seconds=1)
+        >>> # Drive the right wheel a 10 speed units slower than the left for 1 second.
+        >>> randy.drive_forward(80, adjust=-10, seconds=1)
+        >>> # Shutdown randy after he's had his fun.
+        >>> randy.shutdown()
     """
+
     def __init__(self, baud, file=None):
         """
-            Connect and initialize a Geekbot. Connects to a Geekbot via serial using the given
-            baudrate. If the serial port is not given, attempt to find it.
+            Connect and initialize a Geekbot. Connects to a Geekbot via serial.
+
+            baud - The baudrate to use for the serial connection.
+            file - The optional serial port to use for the connection. If not given, we will attempt
+                   to find the right port.
 
             TODO: What is the Arduino code the Geekbot is running? Is it custom? Default provided
             by Trossen? This needs to be documented.
+
+            Example:
+
+            >>> george = Robot(57600)
+            >>> if not george.connected:
+            ...     exit()
+            >>> george.shutdown()
         """
         self.port = serial.Serial()
         if file is not None:
@@ -42,35 +76,35 @@ class Robot(object):
         self.port.timeout = 1
         self.port.dtr = 1
         self.connected = False
+
         if self.location is not None:
             self.port.open()
+            # Give the connection some time to open before we try to use it.
             wait(2)
+            # Tell the Robot we want to connect to it, and wait for it to consent.
             self.port.write(chr(HANDSHAKE).encode())
             wait(0.5)
             while self.port.read() != chr(HANDSHAKE).encode():
                 print("Waiting for handshake")
                 # wait(0.5)
             self.connected = True
+        else:
+            print("Could not connect to Geekbot.")
 
     @staticmethod
     def find_robot():
-        """
-            Attempts to find a serial port with a Geekbot connected.
-        """
+        """Attempts to find a serial port with a Geekbot connected."""
         print("Finding Geekbot's USB port...")
-        port_list = serial.tools.list_ports.comports()
-        for i in port_list:
-            print(i.device)
-            if (i.vid == ROBOT_VID) and (i.pid == ROBOT_PID or i.pid == ROBOT_PID2):
-                print("Geekbot found: " + i.device)
-                return str(i.device)
+        for port in serial.tools.list_ports.comports():
+            print(port.device)
+            if (port.vid == ROBOT_VID) and (port.pid == ROBOT_PID or port.pid == ROBOT_PID2):
+                print("Geekbot found: " + port.device)
+                return str(port.device)
         print("No Geekbot found!")
         return None
 
     def shutdown(self):
-        """
-            Stops all motion and disconnects from the Geekbot.
-        """
+        """Stops all motion and disconnects from the Geekbot."""
         self.halt()
         self.port.close()
         self.connected = False
@@ -99,9 +133,7 @@ class Robot(object):
 
     @staticmethod
     def pack_short(num):
-        """
-            Packs the given int16_t into a struct for use with a serial object.
-        """
+        """Packs the given int16_t into a struct for use with a serial object."""
         # An int16_t in native byte order (little-endian).
         return pack("h", int(num))
 
@@ -115,9 +147,8 @@ class Robot(object):
                 DRIVE_FLAG   - Drive both wheels with equal direction and speed
                 LEFT_FLAG    - Control the left wheel
                 RIGHT_FLAG   - Control the right wheel
-
                 data - An integer value between -100 and 100 whose sign indicates direction, and
-                        whose magnitude indicates speed.
+                       whose magnitude indicates speed.
 
                 LIGHTS_FLAG  - Control the robot's LED
                 data - True: Light on. False: Light off.
@@ -125,12 +156,16 @@ class Robot(object):
                 IR_POS_FLAG  - Set the angle of the IR distance sensor
                 TODO: Verify
                 data - An angle between -360 and 360. Angle is measured from the front of the robot,
-                        angles moving right, and negative angles moving left.
-                TODO: Verify that 0x1 is a placeholder and doesn't have any special meaning.
+                       angles moving right, and negative angles moving left.
+                TODO: Verify that 0x01 is a placeholder and doesn't have any special meaning.
                 IR_READ_FLAG - Request an update from the IR distance sensor
-                data - A placeholder (0x1) should be given.
+                data - A placeholder (0x01) should be given.
         """
+        # Send the command flag and then the data. We send the flag first so that the Geekbot knows
+        # what the data means. Otherwise, it's just a number that could mean anything.
         self.port.write(chr(flag).encode())
+        # The Geekbot expects numbers from SHRT_MIN to SHRT_MAX, but those aren't so great for
+        # teaching young children robotics. So map numbers from [-100, 100] to [SHRT_MIN, SHRT_MAX].
         self.port.write(self.pack_short(self.map_short(data)))
 
     def lights_on(self):
@@ -151,12 +186,13 @@ class Robot(object):
 
             TODO: Make note of special Robotics terminology for this type of locomotion.
 
-            speed - The speed at which to turn the robot. May be between -100 and 100.
+            speed   - The speed at which to turn the robot. May be between -100 and 100.
             seconds - The amount of time the turn should take. May be fractional.
         """
         self.send_cmd(LEFT_FLAG, -speed)
         self.send_cmd(RIGHT_FLAG, speed)
 
+        # We specify how far the robot turns by waiting different lengths of time before stopping.
         if seconds is not None:
             wait(seconds)
             self.halt()
@@ -194,17 +230,39 @@ class Robot(object):
         # Negating both speed and adjust is equivalent to adding, and then negating the result.
         self.drive_forward(-speed, -adjust, seconds)
 
-    def drive_right_wheel(self, speed):
-        """Drives the right wheel indefinitely at the given speed."""
+    def drive_right_wheel(self, speed, seconds=None):
+        """
+            Drives the right wheel.
+
+            speed   - The speed at which to drive the right wheel.
+            seconds - The amount of time the right wheel with drive. If not given, the wheel will
+                      drive at the given speed until instructed otherwise.
+        """
+        # TODO: Document why the speed is negated.
         self.send_cmd(RIGHT_FLAG, -speed)
 
-    def drive_left_wheel(self, speed):
-        """Drives the left wheel indefinitely at the given speed."""
+        if seconds is not None:
+            wait(seconds)
+            self.halt()
+
+    def drive_left_wheel(self, speed, seconds=None):
+        """
+            Drives the left wheel.
+
+            speed   - The speed at which to drive the left wheel.
+            seconds - The amount of time the left wheel with drive. If not given, the wheel will
+                      drive at the given speed until instructed otherwise.
+        """
+        # TODO: Document why the speed is negated.
         self.send_cmd(LEFT_FLAG, -speed)
+
+        if seconds is not None:
+            wait(seconds)
+            self.halt()
 
     def get_ir_distance(self):
         """Gets the distance measured by the Robot's IR sensor."""
-        self.send_cmd(IR_READ_FLAG, 1)
+        self.send_cmd(IR_READ_FLAG, 0x01)
         data = self.port.read(2)
         # A uint16_t, in big-endian byte order.
         dist = unpack(">H", data)
